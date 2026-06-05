414ccff361ac65d8171ec9f2182dafa2e6cca09d
Dr Quill <samquill00@gmail.com>
2026-06-05 04:45:10 +0100

    jazzz more jazz

--- sync_html.py
+++ sync_html.py
@@ -41,7 +41,6 @@ AUDIO_EMBEDS = [
     ("__DSS_DIDOS_LAMENT_DATA_URI__", "didos-lament.mp3",                               "audio/mpeg"),
     ("__DSS_CELLAR_DATA_URI__",      "the-cellar-pump-ambience.m4a",                   "audio/mp4"),
     ("__DSS_QUIET_CAFE_DATA_URI__",  "the-quiet-cafe-ambience.m4a",                    "audio/mp4"),
-    ("__DSS_RONNIES_DATA_URI__",     "the-ronnies-jazz-ambience.m4a",                  "audio/mp4"),
     ("__DSS_RONNIES_MUSIC_DATA_URI__", "ronnies-firebird-jazz.mp3",                    "audio/mpeg"),
     ("__DSS_MINIGAME_DATA_URI__",    "the-minigame-retro.m4a",                         "audio/mp4"),
     ("__DSS_PONGMINI_DATA_URI__",    "the-pongmini-loop.m4a",                          "audio/mp4"),
